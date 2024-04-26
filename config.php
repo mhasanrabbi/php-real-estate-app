ac37ccaa00fb76469b5a871bd35ac75dd5473606
mhasanrabbi <mhmdhasanrabbi@gmail.com>
2024-04-26 12:39:46 +0600

    init project with db config and html template

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbhost = 'localhost';
+$dbname = 'php_real_estate';
+$dbuser = 'root';
+$dbpass = '';
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+    echo "Connection error :" . $exception->getMessage();
+}
+
+define("BASE_URL", "http://localhost/php_real_estate/");
+
+define("SMTP_HOST", "sandbox.smtp.mailtrap.io");
+define("SMTP_PORT", "2525");
+define("SMTP_USERNAME", "302fd4aa1e98a0");
+define("SMTP_PASSWORD", "c0dcea3a4d3607");
---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+
+    <link rel="icon" type="image/png" href="uploads/favicon.png">
+
+    <title>Admin Panel</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/font_awesome_5_free.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/select2.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/bootstrap-tagsinput.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/duotone-dark.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/iziToast.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/fontawesome-iconpicker.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/bootstrap4-toggle.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/components.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/air-datepicker.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/spacing.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/custom.css">
+
+    <script src="<?php echo BASE_URL; ?>dist/js/jquery-3.7.0.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/popper.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/tooltip.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/jquery.nicescroll.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/moment.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/stisla.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/jscolor.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/bootstrap-tagsinput.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/select2.full.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/iziToast.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/fontawesome-iconpicker.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/air-datepicker.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/tinymce/tinymce.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>dist/js/bootstrap4-toggle.min.js"></script>
+</head>
+
+<body>
+<div id="app">
+    <div class="main-wrapper">
